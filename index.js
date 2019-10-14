3209ba9d4093d9bc4815735398149b455d660f74
Raymond Harvey <raymh2002@icloud.com>
2019-10-13 23:42:41 -0400

    add css to hide <input type=radio>; comment <div> and add Answer 2 text <li> to prep for test js

--- index.js
+++ index.js
@@ -36,12 +36,12 @@
 
 
 // quizInProgress
-
-// .js-correct-answer
-$(".js-correct-answer").css({'list-style-image': 'url("./images/Check/Check_16x16.png")',  'background-color': 'lawngreen'})
-    // .js-incorrect-answer
-$(".js-incorrect-answer").css({'list-style-image': 'url("./images/Delete/Delete_16x16.png")', 'background-color': 'red'})
-
-// .results-correct-response-item
-$(".results-correct-response-item").css({'list-style-image': 'url("./images/Arrow/Arrow_16x16.png")'})
+// *** these 3 below have been confirmed to all work to render the defined styling  ***
+// *** I have commented them out below and have uncommented the corresponding css styling entries in index.css ***
+// *** .js-correct-answer ***
+    // $(".js-correct-answer").css({'list-style-image': 'url("./images/Check/Check_16x16.png")',  'background-color': 'lawngreen'})
+// *** .js-incorrect-answer ***
+    // $(".js-incorrect-answer").css({'list-style-image': 'url("./images/Delete/Delete_16x16.png")', 'background-color': 'red'})
+// *** .results-correct-response-item ***
+    // $(".results-correct-response-item").css({'list-style-image': 'url("./images/Arrow/Arrow_16x16.png")'})
 
